efc0cf6b6d9064c68c88e8397fbd7254ef2c6485
Justin Marsan <justin.marsan@linkurio.us>
2022-11-04 15:54:23 +0100

    Setup : Handle box shadows

--- config.js
+++ config.js
@@ -1,8 +1,77 @@
+const StyleDictionary = require("style-dictionary");
+
+StyleDictionary.registerTransform({
+  type: `attribute`,
+  name: `lk/attribute/sizingToSize`,
+  matcher: (token) => {
+    return (
+      token.type === "sizing" ||
+      token.type === "spacing" ||
+      token.type === "borderRadius" ||
+      token.type === "lineHeights" ||
+      token.type === "fontSizes"
+    );
+  },
+  transformer: (token) => {
+    // token.value will be resolved and transformed at this point
+    token.type = "size";
+  },
+});
+
+StyleDictionary.registerTransform({
+  type: `attribute`,
+  name: `lk/size/pxToRem`,
+  matcher: (token) => {
+    return token.type === "size";
+  },
+  transformer: (token) => {
+    // token.value will be resolved and transformed at this point
+    token.value = token.value / 16 + "rem";
+  },
+});
+
+StyleDictionary.registerTransform({
+  type: `value`,
+  name: `lk/dropShadow`,
+  matcher: (token) => {
+    return token.type === "boxShadow";
+  },
+  transformer: (token) => {
+    // token.value will be resolved and transformed at this point
+    const values = [];
+
+    token.value.forEach((shadow) => {
+      values.push(
+        `${shadow.type === "innerShadow" ? "inner" : ""} ${shadow.x}px ${
+          shadow.y
+        }px ${shadow.spread}px ${shadow.color}`
+      );
+    });
+    console.log("join", values.join(", "));
+    return values.join(", ");
+  },
+});
+
+StyleDictionary.registerTransform({
+  type: `attribute`,
+  name: `lk/dump`,
+  transformer: (token) => {
+    // token.value will be resolved and transformed at this point
+    console.log(token);
+  },
+});
+
 module.exports = {
   source: ["style-dic-tokens.json"],
   platforms: {
     css: {
-      transforms: ["attribute/cti", "name/cti/kebab"],
+      transforms: [
+        "lk/attribute/sizingToSize",
+        "attribute/cti",
+        "name/cti/camel",
+        "lk/size/pxToRem",
+        "lk/dropShadow",
+      ],
       files: [
         {
           format: "css/variables",
