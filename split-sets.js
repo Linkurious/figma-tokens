f17a595529113a30c1b1a9c3b1778283fd4cf691
Justin Marsan <justin.marsan@linkurio.us>
2022-11-04 14:58:08 +0100

    Setup : Split Tokens by Set attempt

---
+++ split-sets.js
@@ -0,0 +1,13 @@
+const fs = require("fs");
+
+const tokens = JSON.parse(fs.readFileSync("tokens.json"));
+
+for (const [set, value] of Object.entries(tokens)) {
+  if (set.includes("$")) break;
+
+  console.log("set", set);
+  fs.writeFileSync(
+    "tokens/" + set + ".json",
+    JSON.stringify({ [set]: value }).replaceAll(':"{', ':"{' + set + ".")
+  );
+}
